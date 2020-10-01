ec17190de1fd169f415eb8f200bf0c2f2a5e10a4
michal.jankowski <michaljankowski86@gmail.com>
2020-10-01 16:11:11 +0200

    basic DOM manipulations - properties events and bindings data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-podst-eduweb';
+  title = 'angular-podst';
+
+  constructor() {}
+
+  playlist = {
+    name: 'The best songs',
+    tracks: 23,
+    color: '#ff0000',
+    favourite: true,
+  };
+
+  save(event) {
+    console.log(event);
+  }
 }
