391f346e5c5ef5e5977afd935ebad3044008a6d3
michal.jankowski <michaljankowski86@gmail.com>
2020-10-02 14:55:54 +0200

    dynamic classes, styles and ngModel directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-podst';
+  title = 'ng-podst';
+
+  selected = null;
 
   constructor() {}
 
