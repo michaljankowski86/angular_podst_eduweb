c116eb662da7b8d6f6e0583e09610b3c8b7347d5
michal.jankowski <michaljankowski86@gmail.com>
2020-10-05 22:48:20 +0200

    ngSwitch example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,12 +12,37 @@ export class AppComponent {
 
   edited = {};
 
+  mode = 'none';
+
   playlist = {
     name: 'The best of EduWeb!',
     tracks: 23,
     color: '#FF0000',
     favourite: true,
   };
+
+  select(playlist) {
+    if (playlist == null) {
+      this.mode = 'none';
+      this.selected = null;
+    } else {
+      this.mode = 'selected';
+      this.selected = playlist;
+    }
+  }
+
+  edit(playlist) {
+    this.mode = 'edit';
+    this.edited = playlist;
+    this.selected = playlist;
+  }
+
+  createNew() {
+    this.mode = 'edit';
+    var newPlaylist = {};
+    this.selected = newPlaylist;
+    this.edited = newPlaylist;
+  }
 
   save(event) {
     console.log('Zapisano', event);
