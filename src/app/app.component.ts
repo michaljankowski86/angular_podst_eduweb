c7ae2a065e4524d024b27a4237a418d9e18e2996
michal.jankowski <michaljankowski86@gmail.com>
2020-10-08 14:44:34 +0200

    playlists component - app-root component refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,55 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Witaj w kursie Angular!';
-
-  selected = null;
-
-  edited = {};
-
-  mode = 'none';
-
-  playlists = [
-    {
-      name: 'Angular Greatest Hits!',
-      tracks: 2,
-      color: '#FF0000',
-      favourite: true,
-    },
-    {
-      name: 'The best of EduWeb!',
-      tracks: 23,
-      color: '#0000FF',
-      favourite: false,
-    },
-  ];
-
-  select(playlist) {
-    if (playlist !== this.selected) this.mode = 'selected';
-    this.selected = playlist;
-  }
-
-  edit(playlist) {
-    this.mode = 'edit';
-    this.edited = playlist;
-    this.selected = playlist;
-  }
-
-  createNew() {
-    this.mode = 'edit';
-    var newPlaylist = {};
-    this.selected = newPlaylist;
-    this.edited = newPlaylist;
-  }
-
-  save(event) {
-    console.log('Zapisano', event);
-  }
 
   constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FormsModule } from '@angular/forms';
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { ContentCardComponent } from './playlists/content-card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PlaylistsComponent, ContentCardComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
